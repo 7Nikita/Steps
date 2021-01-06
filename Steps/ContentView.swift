260a77ed310553e844bb0b6316c0ce4ffff93c1f
Nikita Pekurin <nikita.mda@yandex.by>
2021-01-06 15:32:50 +0300

    Initial Commit

---
+++ Steps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Steps
+//
+//  Created by Nikita Pekurin on 6.01.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Steps/StepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepsApp.swift
+//  Steps
+//
+//  Created by Nikita Pekurin on 6.01.21.
+//
+
+import SwiftUI
+
+@main
+struct StepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
